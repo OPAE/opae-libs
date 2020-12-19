831c29bde4c26f1ac7563020b72a49b254cecf51
Tim Whisonant <tim.whisonant@intel.com>
2020-12-18 17:20:04 -0800

    Update mem_alloc.h

--- include/opae/mem_alloc.h
+++ include/opae/mem_alloc.h
@@ -25,6 +25,19 @@
 // POSSIBILITY OF SUCH DAMAGE.
 #ifndef __OPAE_MEM_ALLOC_H__
 #define __OPAE_MEM_ALLOC_H__
+
+/**
+* Provides an API for allocating/freeing a logical address space. There
+* is no interaction with any OS memory allocation infrastructure, whether
+* malloc, mmap, etc. The "address ranges" tracked by this allocator are
+* arbitrary 64-bit integers. The allocator simply provides the bookeeping
+* logic that ensures that a unique address with the appropriate size is
+* returned for each allocation request, and that an allocation can be freed,
+* ie released back to the available pool of logical address space for future
+* allocations. The memory backing the allocator's internal data structures
+* is managed by malloc()/free().
+*/
+
 #include <stdint.h>
 
 struct mem_link {
